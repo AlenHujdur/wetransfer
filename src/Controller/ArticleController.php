2ad6cde4f2883ca28d377bff99867e133cac8ebc
hujduralen <alen@sigura.eu>
2018-12-25 16:08:40 +0100

    twig

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,10 +2,12 @@
 
 namespace App\Controller;
 
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+// use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
-class ArticleController
+class ArticleController extends AbstractController
 { 
 
 /**
@@ -20,7 +22,16 @@ public function homepage()
 */
 public function show($slug)
 {
-    return new Response(sprintf('Future page to show the article: %s', $slug));
+    $comments = [
+        'I ate a normal rock once. It did NOT taste like bacon!',
+        'Woohoo! I\'m going on an all-asteroid diet!',
+        'I like bacon too! Buy some from my site! bakinsomebacon.com',
+    ];
+    // return new Response(sprintf('Future page to show the article: %s', $slug));
+    return $this->render('article/show.html.twig', [
+        'title' => ucwords(str_replace('-',' ', $slug)),
+        'comments'=> $comments,
+    ]);
 }  
 
 }
